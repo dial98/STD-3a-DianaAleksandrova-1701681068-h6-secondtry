f5b5955aea9b6c5893d53e9b6a4f0d097bcc4ac9
dial98 <dial98>
2017-11-05 22:16:32 +0200

    Homework week 6 second try all tasks done

---
+++ GeometryCalculator/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GeometryCalculator
+{
+    class Program
+    {
+        static double triangleArea(double side, double height)
+           {            
+            return (side*height)/2;
+           }
+        static double squareArea(double side1)
+           {
+            return (side1 * side1);
+           }
+        static double getRadians(double degrees,double radians)
+           {
+            return (Math.PI/180*degrees);
+           }
+
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Choose option (triangle, square or radians):");
+            string figureType = Console.ReadLine().ToLower();
+                     
+                   
+            switch (figureType)
+            {
+                case "triangle":
+                    Console.WriteLine("Enter side:");
+                    int side = int.Parse(Console.ReadLine());
+                    Console.WriteLine("Enter height:");
+                    int height = int.Parse(Console.ReadLine());
+                    Console.WriteLine("The area of the {0} is:{1:f2}","triangle", triangleArea(side, height));
+                    break;
+
+                case "square":
+                    Console.WriteLine("Enter side:");
+                    int side1 = int.Parse(Console.ReadLine());
+                    Console.WriteLine("Enter height:");
+                    int height1 = int.Parse(Console.ReadLine());
+                    Console.WriteLine("The area of the {0} is: {1:f2}","square",squareArea(side1));
+                    break;
+
+                case "radians":
+
+                    Console.WriteLine("Enter the degrees you want to transform in radians:");
+                    double degrees = double.Parse(Console.ReadLine());
+                    double radians=0;
+                    Console.WriteLine("Your degrees in radians are:{0}",getRadians(degrees,radians));
+                    break;
+            }
+            
+            
+            
+        }
+    }
+}
---
+++ GetMax/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GetMax
+{
+    class Program
+    {
+        static int  GetMax (int firstNum, int secondNum, int thirdNum)
+        {
+            int maxNum=firstNum;
+            if (firstNum < secondNum)
+            {
+                maxNum=secondNum;
+            }
+            if (secondNum < thirdNum)
+            {
+                 maxNum=thirdNum;
+            }
+
+            else
+            { thirdNum = maxNum; }
+            return maxNum;
+
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter first number here:");
+            int numOne = int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter second number here:");
+            int numTwo = int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter third number here:");
+            int numThree = int.Parse(Console.ReadLine());
+            Console.WriteLine("The biggest number of this three is: {0} ", GetMax(numOne, numTwo, numThree)); 
+        }
+    }
+}
